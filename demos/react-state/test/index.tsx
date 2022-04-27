fcb8d2ac49a9f599134946175e0ea4c8a6620265
wenjiawei01 <wenjiawei01@58.com>
2022-04-27 15:01:17 +0800

    feat: 输入框光标插入内容 demo

--- demos/react-state/test/index.tsx
+++ demos/react-state/test/index.tsx
@@ -4,15 +4,19 @@ import ReactDOM from 'react-dom/client';
 import HooksCom from './components/hooksCom';
 import ClassCom from './components/classCom';
 
+import InputTest from './components/InputTest';
+
 import './index.less';
 import 'antd/dist/antd.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className='wrap'>
         <HooksCom />
         <ClassCom />
+
+        <InputTest />
       </div>
     );
   }
