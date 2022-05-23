92622cf0686d69deceecd2efad876a86c308c30d
wenjiawei01 <wenjiawei01@58.com>
2022-05-23 10:17:27 +0800

    feat: node event vm 测试code

--- demos/Node/src/event/index.js
+++ demos/Node/src/event/index.js
@@ -1,41 +1,41 @@
+// NOTE 绑定多个事件回调
+
 // const EventEmitter = require('events');
-
 // let emitter = new EventEmitter();
-
 // emitter.on('myEvent', () => {
 //   console.log('hi 1');
 // });
-
 // emitter.on('myEvent', () => {
 //   console.log('hi 2');
 // });
-
 // emitter.emit('myEvent');
 
+// NOTE 循环触发
+// const EventEmitter = require('events');
+// let emitter = new EventEmitter();
+// let i = 10;
+// emitter.on('myEvent', () => {
+//   if (i <= 0) {
+//     return;
+//   }
+//   i--;
+//   console.log('hi');
+//   emitter.emit('myEvent');
+// });
+// emitter.emit('myEvent');
 
+// NOTE
 // const EventEmitter = require('events');
 
 // let emitter = new EventEmitter();
 
-// emitter.on('myEvent', () => {
-//   console.log('hi');
-//   emitter.emit('myEvent');
-// });
-
+// let i = 0;
+// function sth (mark) {
+//   console.log('hi', mark);
+//   i++;
+//   emitter.on('myEvent', sth.bind(this, i));
+// }
+// emitter.on('myEvent', sth.bind(this, i));
 // emitter.emit('myEvent');
-
-
-const EventEmitter = require('events');
-
-let emitter = new EventEmitter();
-
-let i = 0;
-function sth (mark) {
-  console.log('hi', mark);
-  i++;
-  emitter.on('myEvent', sth.bind(this, i));
-}
-emitter.on('myEvent', sth.bind(this, i));
-emitter.emit('myEvent');
-emitter.emit('myEvent');
-emitter.emit('myEvent');
+// emitter.emit('myEvent');
+// emitter.emit('myEvent');
--- demos/Node/src/vm/index.js
+++ demos/Node/src/vm/index.js
@@ -1,6 +1,6 @@
 const vm = require('vm')
 let name = 'Node';
-global.name = 'G:Node';
+// global.name = 'G:Node';
 let str = 'console.log(name)';
-// vm.runInThisContext(str); // name is not defined
-vm.runInNewContext(str, global); // name is not defined
+vm.runInThisContext(str); // name is not defined
+// vm.runInNewContext(str, global); // name is not defined
