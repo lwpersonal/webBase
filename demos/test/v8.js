9fe171370a264ec8da42b2862d3b1d952cb84cd5
wenjiawei01 <wenjiawei01@58.com>
2023-02-01 16:47:58 +0800

    feat: v8

---
+++ demos/test/v8.js
@@ -0,0 +1,14 @@
+let a = { x: 1 };
+function bar(obj) {
+  return obj.x;
+}
+
+function foo() {
+  let ret = 0;
+  for (let i = 1; i < 7049; i++) {
+    ret += bar(a);
+  }
+  return ret;
+}
+
+foo();
