6e5b10abe62df8568791189f0ef07d951edaeb3d
wenjiawei01 <wenjiawei01@58.com>
2023-07-10 14:58:45 +0800

    feat: test code

--- demos/test/test.js
+++ demos/test/test.js
@@ -1,22 +1,8 @@
-// function curry(fn, len = fn.length) {
-//   return function handler(...args) {
-//     if (args.length >= len) {
-//       return fn.call(this, ...args);
-//     } else {
-//       return handler.bind(this, ...args);
-//     }
-//   };
-// }
-
-// function add(a, b, c = 0) {
-//   return a + b + c;
-// }
-// function main() {
-//   const fn = curry(add, 3);
-//   console.log(fn(1)(2, 3));
-//   console.log(fn(1)(2)(10));
-// }
-
-// main();
-
-Promise.all([1, () => 2, Promise.resolve(3)]).then(res => console.log(res));
+console.log('-----');
+setTimeout(() => {
+  console.log('setTimeout');
+}, 0);
+Promise.resolve().then(() => {
+  console.log('Promise');
+});
+process.nextTick(() => console.log('nextTick'));
